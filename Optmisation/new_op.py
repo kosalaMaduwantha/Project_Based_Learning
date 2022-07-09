79e95c65ee5b1fdd319960cbd164d0e181180246
kosalaMaduwantha <kosalamaduwantha0@gmail.com>
2022-07-09 16:53:46 +0530

    adding new files

--- Optmisation/new_op.py
+++ Optmisation/new_op.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from ypstruct import structure
+import ga
 
 
 ## cost function
@@ -15,7 +16,14 @@ problem.nvar = 5
 problem.varmin = -10 ## lower bound
 problem.varmax = 10 ## upper bound
 
-#
+## defining parameters for GA
+params = structure()
+params.maxit = 100
+params.npop = 20
 
+print(0)
 
+## run ga
+ga.run(problem, params)
 
+print(0)
